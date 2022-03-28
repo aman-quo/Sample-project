d1ddfcd5cf7bea8c8de5f2707483f03b89834f93
Manish Kumar <manish.kumar1@quovantis.com>
2022-03-28 18:38:25 +0530

    feat:Addes testing for frontend

--- frontend/src/app/service/user/user.service.ts
+++ frontend/src/app/service/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment'
 import { HttpService } from '../http/http.service';
 
